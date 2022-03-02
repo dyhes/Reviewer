ec5786195ee24e55a8ad13dba6558aa2694bfa4b
dyhes <1325574784@qq.com>
2022-03-02 20:51:50 +0800

    1.0.7

--- components/todo.js
+++ components/todo.js
@@ -26,7 +26,7 @@ export default function Todo({todo,handleCount}){
                   <Button type="primary" onClick={handleClick}>完成</Button>
                 </div>} style={{ width:300}}>
                 <p>备注：{todo.remark.length===0?'无':todo.remark}</p>
-                <p>链接：{todo.link.length===0?'无':<a href={todo.link} target='_blank'>{todo.task}</a>}</p>
+                <p>链接：{todo.link.length===0?'无':<a href={todo.link} target='_blank' rel="noreferrer">{todo.task}</a>}</p>
               </Card>
               </List.Item>:<></>}</>
     )
